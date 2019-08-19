f6617b070fcbd1ec8bcb1f6fa13b0d39efc561b8
C21Josiah.Goosen <C21Josiah.Goosen@usafa.edu>
2019-08-19 14:37:57 -0600

    initial push

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+import java.util.Scanner;
+
+public class Main {
+    /**
+     *
+     * @param args - command line argument
+     */
+    public static void main(String[] args) {
+
+	    System.out.println("My HW1.");
+
+    }
+}
