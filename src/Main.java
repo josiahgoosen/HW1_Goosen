06dd0ddebdb6e447f1245d464637e6972b0a73e9
C21Josiah.Goosen <C21Josiah.Goosen@usafa.edu>
2019-08-21 14:30:36 -0600

    added play again

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@
  * and catch here https://www.w3schools.com/java/java_try_catch.asp
  * 4) Learned how to generate a random number here
  * https://stackoverflow.com/questions/5271598/java-generate-random-number-between-two-given-values, copied the code
- * 5) Learned how to use switch statements on https://www.geeksforgeeks.org/switch-statement-in-java/
+ * 5)
  *
  */
 import java.util.Scanner;
@@ -22,40 +22,46 @@ public class Main {
      * @param args - command line argument
      */
     public static void main(String[] args) {
-
-        System.out.println("Welcome to the High Low Game!");
-        String name = getName();
-	    System.out.print("Welcome, " +name);
-        System.out.println(", guess a number between 1 and 100");
-	    boolean rightAnswer = false;
-	    int guessCount = 0;
-	    int low = 1;
-	    int high = 100;
-        int random = (int) (Math.random() * (high - low)) + low;
-	    while (!rightAnswer) {
-            int guess = getGuess();
-            guessCount++;
-            if (guess > high){
-                System.out.println("You guessed higher than high end of the range. You bufoon.");
+        boolean answer = true;
+        do{
+            System.out.println("Welcome to the High Low Game!");
+            String name = getName();
+            System.out.print("Welcome, " +name);
+            System.out.println(", guess a number between 1 and 100");
+            boolean rightAnswer = false;
+            int guessCount = 0;
+            int gameCount = 0;
+            int low = 1;
+            int high = 100;
+            int random = (int) (Math.random() * (high - low)) + low;
+            while (!rightAnswer) {
+                int guess = getGuess();
+                guessCount++;
+                if (guess > high){
+                    System.out.println("You guessed higher than high end of the range. You bufoon.");
+                }
+                else if (guess > random){
+                    System.out.println("Too high, guess again.");
+                    high = guess;
+                }
+                else if (guess < low){
+                    System.out.println("You guessed lower than the low end of the range. Not the sharpest tool in the shed, are ya?");
+                }
+                else if (guess < random){
+                    System.out.println("Too low, guess again.");
+                    low = guess;
+                }
+                else{
+                    System.out.println("You won!");
+                    rightAnswer = true;
+                }
             }
-            else if (guess > random){
-                System.out.println("Too high, guess again.");
-                high = guess;
-            }
-            else if (guess < low){
-                System.out.println("You guessed lower than the low end of the range. Not the sharpest tool in the shed, are ya?");
-            }
-            else if (guess < random){
-                System.out.println("Too low, guess again.");
-                low = guess;
-            }
-            else{
-                System.out.println("You won!");
-                rightAnswer = true;
-            }
-        }
-
+            answer = playAgain();
+            gameCount++;
+        }while(answer);
+        System.out.println("Thanks for playing!");
     }
+
 
     /**
      *
@@ -91,5 +97,31 @@ public class Main {
             }
         }while (goodGuess == 0);
         return guess;
+    }
+    private static boolean playAgain(){
+        String answer;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Would you like to play again?");
+        answer = scan.nextLine();
+        switch (answer) {
+            case "y":
+                return true;
+            case "yes":
+                return true;
+            case "Yes":
+                return true;
+            case "Y":
+                return true;
+            case "n":
+                return false;
+            case "N":
+                return false;
+            case "no":
+                return false;
+            case "No":
+                return false;
+            default:
+                return false;
+        }
     }
 }
